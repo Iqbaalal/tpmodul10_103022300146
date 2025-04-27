d76698363f31a9fca1e6f1583bb4f2c63a03fd66
baalaal <147219396+baalaal@users.noreply.github.com>
2025-04-28 02:49:13 +0700

    Add project files.

---
+++ Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpmodul10_1302000001.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
---
+++ MahasiswaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul10_1302000001.Models;
+
+namespace tpmodul10_1302000001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswas = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Muhammad Iqbal", Nim = "103022300146" },
+            new Mahasiswa { Nama = "Stephen Curry", Nim = "1302000002" },
+            new Mahasiswa { Nama = "LeBron James", Nim = "1302000003" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswas;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswas[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswas.Add(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mahasiswas.RemoveAt(id);
+        }
+    }
+}
